325a31ec06a1f95f96bcb07addbca9c883be8d7b
yas-wanth <arjunraochirumamilla@gmail.com>
2025-03-05 12:21:21 +0530

    Update Yaswanth.py

--- Yaswanth.py
+++ Yaswanth.py
@@ -1,8 +1,8 @@
 # Simple Addition Program
 
 # Get user input
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = 21
+num2 = 24
 
 # Perform addition
 result = num1 + num2
